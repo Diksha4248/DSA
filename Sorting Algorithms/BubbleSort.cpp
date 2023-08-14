7fc347bbdc764603b931230de7e713d97d772717
Diksha4248 <88928334+Diksha4248@users.noreply.github.com>
2023-08-15 01:44:45 +0530

    Bubble sort added

---
+++ Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(N2)
+// Auxiliary Space: O(1)
+
+void BubbleSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for(int j =0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+        }
+    }
+
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+int main(){
+
+    int arr[] = {23,56,2,1,24};
+    int n = sizeof(arr)/sizeof(int);
+
+    BubbleSort(arr,n);
+
+    return 0;
+}
